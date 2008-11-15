68adde58dd4f396ffced018005868ca7c3d49220
hybris@o2.pl <hybris@o2.pl@c9278602-9774-11dd-b35e-65caead18e0b>
2008-11-15 18:54:21 +0000

    klasa Config w wersji rc1

--- src/Config.java
+++ src/Config.java
@@ -1,72 +1,166 @@
+package jreader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+public class Config implements Serializable {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Sciezka do katalogu konfiguracyjnego
+	 */
+	private static File configDir = new File(System.getenv("HOME") + File.separator + ".jreader");
+	/**
+	 * Sciezka do pliku zawierajacego ustawienia programu
+	 */
+	private static File configFile = new File(Config.configDir.getPath() + File.separator + "config");
+	/**
+	 * Obiekt zawierajacy ustawienia programu
+	 */
+	private static ConfigData cd = null;
+	
+	Config() { 
+		Config.configDir.mkdirs();
+		if(Config.cd == null) {
+			if(!read()) {
+				Config.cd = new ConfigData();
+				write();
+			}
+		}
+	}
+	/**
+	 * metoda zwraca sciezke do katalogu konfiguracyjnego
+	 * @return sciezka do katalogu typu String lub pusty String, gdy sciezki nie zdefiniowano
+	 */
+	public String getConfDir() {
+		return Config.configDir.getPath();
+	}
+	/**
+	 * metoda zwraca sciezke do pliku z ustawienia programu
+	 * @return sciezka do pliku typu String lub pusty String, gdy sciezka nie jest zdefiniowana
+	 */
+	public String getConfFile() {
+		return Config.configFile.getPath();
+	}
 
-/**
- * Zawiera ustawienia programu, wybrane przez uzytkownika.
- */
-class Config implements Serializable {
-  /**
-   * Kolejnosc sortowania elementow.
-   * Czy najnowsze maja byc najpierw; jesli false, to najstarsze najpierw.
-   */
-  private static boolean sortByNewest;
-  /*
-   * Czy aktualizowac wszystkie kanaly automatycznie przy starcie programu.
-   */
-  // TODO
-  private static boolean updateAllOnStartup;
-  /*
-   * Co ile minut automatycznie aktualizowac wszystkie kanaly.
-   * Wartosc 0 wylacza ta opcje.
-   */
-  // TODO
-  private static int autoUpdateMinutes;
-  /*
-   * Usuwa wiadomosci starsze niz dana ilosc dni.
-   * Wartosc 0 wylacza ta opcje.
-   */
-  // TODO
-  private static int deleteOlderThanDays;
-
-  Config() {
-    sortByNewest = true;
-    updateAllOnStartup = false;
-    autoUpdateMinutes = 0;
-    deleteOlderThanDays = 0;
-  }
-
-
-  static boolean getSortByNewest() {
-    return sortByNewest;
-  }
-
-  static boolean getUpdateAllOnStartup() {
-    return updateAllOnStartup;
-  }
-
-  static int getAutoUpdateMinutes() {
-    return autoUpdateMinutes;
-  }
-
-  static int getDeleteOlderThanDays() {
-    return deleteOlderThanDays;
-  }
-
-
-  static void setSortByNewest(boolean b) {
-    sortByNewest = b;
-  }
-
-  static void setUpdateAllOnStartup(boolean b) {
-    updateAllOnStartup = b;
-  }
-
-  static void setAutoUpdateMinutes(int m) {
-    autoUpdateMinutes = m;
-  }
-
-  static void setDeleteOlderThanDays(int d) {
-    deleteOlderThanDays = d;
-  }
+	public boolean getSortByNewest() {
+		return Config.cd.sortByNewest;
+	}
+	
+	public boolean getUpdateAllOnStartup() {
+		return Config.cd.updateAllOnStartup;
+	}
+	
+	public int getAutoUpdateMinutes() {
+		return Config.cd.autoUpdateMinutes;
+	}
+	
+	public int getDeleteOlderThanDays() {
+		return Config.cd.deleteOlderThanDays;
+	}
+	
+	public void setSortByNewest(boolean b) {
+		Config.cd.sortByNewest = b;
+	}
+	
+	public void setUpdateAllOnStartup(boolean b) {
+		Config.cd.updateAllOnStartup = b;
+	}
+	
+	public void setAutoUpdateMinutes(int m) {
+		Config.cd.autoUpdateMinutes = m;
+	}
+	
+	public void setDeleteOlderThanDays(int d) {
+		Config.cd.deleteOlderThanDays = d;
+	}
+	/**
+	 * metoda zapisuje biezace ustawienia programu do pliku 
+	 * @return zwraca true, gdy operacja sie powiedzie lub false w przeciwnym wypadku
+	 */
+	public boolean write() {
+		ObjectOutputStream oos = null;
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(Config.configFile);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(Config.cd);
+		} catch (Exception e) {
+			System.err.println(e);
+			return false;
+		} finally {
+			try {
+				if(oos != null)
+					oos.close();
+				if(fos != null)
+					fos.close();
+			} catch(Exception e) {
+				System.err.println(e);
+			}
+		}
+		return true;
+	}
+	/**
+	 * metoda wczytuje ustawienia programu z pliku
+	 * @return zwraca true, gdy operacja sie powiedzie lub false w przeciwnym wypadku
+	 */
+	public boolean read() {
+		ObjectInputStream ois = null;
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(Config.configFile);
+			ois = new ObjectInputStream(fis);
+			Config.cd = (ConfigData) ois.readObject();
+		} catch (Exception e) {
+			System.err.println(e);
+			return false;
+		} finally {
+			try {
+				if(ois != null)
+					ois.close();
+				if(fis != null)
+					fis.close();
+			} catch(Exception e) {
+				System.err.println(e);
+			}
+		}
+		return true;
+	}
+	
+	private class ConfigData implements Serializable {
+		private static final long serialVersionUID = 1L;
+		/**
+		 * Kolejnosc sortowania elementow.
+		 * Czy najnowsze maja byc najpierw; jesli false, to najstarsze najpierw.
+		 */
+		public boolean sortByNewest;
+	    /**
+	     * Czy aktualizowac wszystkie kanaly automatycznie przy starcie programu.
+	     */
+	    public boolean updateAllOnStartup;
+	    /**
+	     * Co ile minut automatycznie aktualizowac wszystkie kanaly.
+	     * Wartosc 0 wylacza ta opcje.
+	     */
+	    public int autoUpdateMinutes;
+	    /**
+	     * Usuwa wiadomosci starsze niz dana ilosc dni.
+	     * Wartosc 0 wylacza ta opcje.
+	     */
+	    public int deleteOlderThanDays;
+		
+		public ConfigData() {
+			super();
+			/**
+			 * Konfiguracja domyślna
+			 */
+			this.sortByNewest = true;
+			this.updateAllOnStartup = false;
+			this.autoUpdateMinutes = 0;
+			this.deleteOlderThanDays = 10;
+		}
+	}
 }
-
