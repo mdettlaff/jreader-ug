2e124e802876155d96d09808c2e4a99cc059579e
kajoj87 <kajoj87@c9278602-9774-11dd-b35e-65caead18e0b>
2008-12-05 21:56:33 +0000

    dodalem oblusge importu subskrypcji w menu File

--- trunk/swt/JReader/MainSash.java
+++ trunk/swt/JReader/MainSash.java
@@ -25,23 +25,19 @@ public class MainSash {
 	
 	public MainSash(Composite shell) {
 		
-				
-		/*
-		 * Create the page.  This example does not use layouts.
-		 */
 		sashComp = new Composite(shell, SWT.NONE);
 		sashComp.setLayoutData (new GridData (SWT.FILL, SWT.FILL, true, true));
 		
-		/* Create the list and text widgets */
+		/* Tworze nowe taby */
 		folderFilter = new Filters(sashComp); 
 		folderItem = new Items(sashComp);
 		folderPreview = new Preview(sashComp);
 	
-		/* Create the sashes */
+		/* Dwie sashes */
 		vSash = new Sash (sashComp, SWT.VERTICAL | SWT.SMOOTH);
 		hSash = new Sash (sashComp, SWT.HORIZONTAL | SWT.SMOOTH);
 		
-		/* Add the listeners */
+		/* listeners */
 		hSash.addSelectionListener (new SelectionAdapter () {
 			public void widgetSelected (SelectionEvent event) {
 				Rectangle rect = vSash.getParent().getClientArea();
@@ -76,18 +72,11 @@ public class MainSash {
 		Rectangle clientArea = sashComp.getClientArea ();
 		Rectangle hSashBounds = hSash.getBounds ();
 		Rectangle vSashBounds = vSash.getBounds ();
-		//System.out.println("hSashBounds: " + hSashBounds.x + " " + hSashBounds.y + " " + hSashBounds.width + " " + hSashBounds.height);
-		//System.out.println("vSashBounds: " + vSashBounds.x + " " + vSashBounds.y + " " + vSashBounds.width + " " + vSashBounds.height);
 		
 		folderFilter.setBounds (0, 0, vSashBounds.x, folderFilterHeight);
 		folderItem.setBounds (vSashBounds.x + vSashBounds.width, 0, clientArea.width - (vSashBounds.x + vSashBounds.width), hSashBounds.y);
 		folderPreview.setBounds (vSashBounds.x + vSashBounds.width, hSashBounds.y + hSashBounds.height, clientArea.width - (vSashBounds.x + vSashBounds.width), clientArea.height - (hSashBounds.y + hSashBounds.height));
 	
-		/**
-		* If the horizontal sash has been moved then the vertical
-		* sash is either too long or too short and its size must
-		* be adjusted.
-		*/
 		hSashBounds.width = clientArea.width - vSashBounds.x;
 		hSashBounds.x = vSashBounds.x;
 		hSash.setBounds (hSashBounds);
@@ -95,29 +84,27 @@ public class MainSash {
 	
 	void resized () {
 		
-		/* Get the client area for the shell */
 		Rectangle clientArea = sashComp.getClientArea ();
 		
 		/*
-		* Make list 1 half the width and half the height of the tab leaving room for the sash.
-		* Place list 1 in the top left quadrant of the tab.
+		* Tworzy bounds dla tabu 'filters'. Umieszcza go po lewej stronie.
+		* Wysokoœæ z góry ustalona.
+		* (x, y, width, height)
 		*/
 		Rectangle list1Bounds = new Rectangle (0, 0, (clientArea.width - SASH_WIDTH) / 4, folderFilterHeight);
 		folderFilter.setBounds (list1Bounds);
 	
 		/*
-		* Make list 2 half the width and half the height of the tab leaving room for the sash.
-		* Place list 2 in the top right quadrant of the tab.
+		* Tworzy tab items gora z prawej.
 		*/
 		folderItem.setBounds (list1Bounds.width + SASH_WIDTH, 0, clientArea.width - (list1Bounds.width + SASH_WIDTH), folderItemHeight);
 	
 		/*
-		* Make the text area the full width and half the height of the tab leaving room for the sash.
-		* Place the text area in the bottom half of the tab.
+		* Tworzy tab preview tak samo jak items tyle ze na dole
 		*/
 		folderPreview.setBounds (list1Bounds.width + SASH_WIDTH, 100 + SASH_WIDTH, clientArea.width - (list1Bounds.width + SASH_WIDTH), clientArea.height - (100 + SASH_WIDTH));
 	
-		/* Position the sashes */
+		/* Pozycjonowanie sashes */
 		vSash.setBounds (list1Bounds.width, 0, SASH_WIDTH, clientArea.height);
 		hSash.setBounds (list1Bounds.width, 100, clientArea.width - list1Bounds.width, SASH_WIDTH);
 	}
