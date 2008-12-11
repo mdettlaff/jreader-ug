0705437c840e0ffa00f2eba04c0786a7c1bb7754
hybris@o2.pl <hybris@o2.pl@c9278602-9774-11dd-b35e-65caead18e0b>
2008-12-11 21:41:32 +0000

    poprawilem wszystko o co mnie prosiles w r60, przeczytaj komentarz :)

--- src/jreader-lu/Channel.java
+++ src/jreader-lu/Channel.java
@@ -3,39 +3,56 @@ package jreader;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Klasa przechowuje informacje o kanale
+ */
 public class Channel implements Serializable {
 	private static final long serialVersionUID = 1L;
 	/**
-	 * Lista ID elementow nalezace do kanalu
+	 * Lista identyfikatorów elementów należących do kanału
 	 */
 	private List<String> items;
     /**
-     * Tagi; jeśli kanał nie jest oznaczony tagami, lista jest pusta.
+     * Tagi; jeśli kanał nie jest oznaczony tagami, lista jest pusta
      */
-    private List<String> tags;
+	private List<String> tags;
 	/**
 	 * URL pliku XML z treścią kanału.
 	 */
-    private String channelURL;
+	private String channelURL;
     /**
-     * Unikalny identyfikator kanalu
+     * Unikalny identyfikator kanału
      */
-    private String id;
+	private String id;
     /** 
-     * true, jeśli ostatnia próba aktualizowania kanału nie powiodła się.
+     * Informacja o nieudanej aktualizacji kanału
      */
-    private boolean isFail;
-
-    private String title;
-    private String link;
-    private String description;
-    private String imageURL;
-    private String imageTitle;
-    private String imageLink;
+	private boolean isFail;
 	/**
-	 * Konstruktor inicjuje podstawowe wartosci kanalu
-	 * @param channelURL - URL kanalu
+	 * Tytuł kanału
+	 */
+	private String title;
+	/**
+	 * Odnośnik do strony z której został pobrany kanał
+	 */
+	private String link;
+	/**
+	 * Opis kanału
+	 */
+	private String description;
+	/* nie wiem co to ma byc */
+	private String imageURL;
+	/* nie wiem co to ma byc */
+	private String imageTitle;
+	/* nie wiem co to ma byc */
+	private String imageLink;
+	/**
+	 * Ilość nieprzeczytanych elementów
+	 */
+	private int unreadItemsCount;
+	/**
+	 * Inicjuje podstawowe wartości kanału
+	 * @param channelURL adres kanału
 	 */
 	public Channel(String channelURL) {
 		this.id = channelURL;
@@ -43,129 +60,130 @@ public class Channel implements Serializable {
 		this.tags = new ArrayList<String>();
 		this.channelURL = channelURL;
 		this.isFail = false;
+		this.unreadItemsCount = 0;
 	}
 	/**
-	 * Metoda dodaje elementu (identyfikator) do kanalu
-	 * @param itemId - idetyfikator elementu
+	 * Dodaje identyfikator element do kanału
+	 * @param itemId idetyfikator elementu
 	 */
 	public void addItem(String itemId) {
 		this.items.add(itemId);
 	}
 	/**
-	 * Metoda zwraca liste wszystkich elementow (identyfikatory) w kanale
-	 * @return zwraca liste elementow (identyfikatory)
+	 * Zwraca listę wszystkich identyfikatorów elementów w kanale
+	 * @return zwraca listę identyfikatorów elementów
 	 */
 	public List<String> getItems() {
 		return this.items;
 	}
 	/**
-	 * Metoda ustawia elementy, ktore maja nalezec do kanalu
-	 * @param items - lista elementow
+	 * Ustawia nową listę idetyfikatorów elementów, które mają należeć do kanału
+	 * @param items lista elementow
 	 */
 	public void setItems(List<String> items) {
 		this.items = items;
 	}
 	/**
-	 * Metoda usuwa element o podanym identyfikatorze
-	 * @param itemId - identyfikator elementu
+	 * Usuwa idetyfikator elementu z listy
+	 * @param itemId identyfikator elementu
 	 */
 	public void removeItem(String itemId) {
 		this.items.remove(itemId);
 	}
 	/**
-	 * Metoda dodaje tag do kanalu
-	 * @param tag - dodawany tag
+	 * Dodaje tag do listy tagów
+	 * @param tag dodawany tag
 	 */
 	public void addTag(String tag) {
 		this.tags.add(tag);
 	}
 	/**
-	 * Metoda zwraca liste wszystkich tagow
-	 * @return lista tagow,
+	 * Zwraca listę wszystkich tagów
+	 * @return lista tagów,
 	 */
 	public List<String> getTags() {
 		return this.tags;
 	}
 	/**
-	 * Metoda ustawia tagi
-	 * @param tags - lista tagow
+	 * Ustawia nową listę tagów
+	 * @param tags lista tagów
 	 */
 	public void setTags(List<String> tags) {
 		this.tags = tags;
 	}
 	/**
-	 * Metoda usuwa tag
-	 * @param tag - usuwany tag
+	 * Usuwa tag z listy tagów
+	 * @param tag usuwany tag
 	 */
 	public void removeTag(String tag) {
 		this.tags.remove(tag);
 	}
 	/**
-	 * Metoda zwraca URL kanalu
-	 * @return zwraca URL kanalu
+	 * Zwraca adres kanału
+	 * @return zwraca adres kanału
 	 */
 	public String getChannelURL() {
 		return this.channelURL;
 	}
 	/**
-	 * Metoda zwraca identyfikator kanalu
-	 * @return zwraca identyfikator kanalu
+	 * Zwraca identyfikator kanału
+	 * @return zwraca identyfikator kanału
 	 */
 	public String getId() {
 		return this.id;
 	}
 	/**
-	 * Metoda sprawdza czy ostatnia aktualizacja kanalu sie nie powiodła
-	 * @return zwraca true gdy ostatnia aktualizacja sie nie powidła, false w przeciwnym wypadku
+	 * Sprawdza, czy ostatnia aktualizacja kanału się nie powiodła
+	 * @return zwraca true, gdy ostatnia aktualizacja się nie powidła, false w przeciwnym wypadku
 	 */
 	public boolean isFail() {
 		return this.isFail;
 	}
 	/**
-	 * Metoda ustawi stan kanalu podczas aktualizacji
-	 * @param isFail true, gdy sie nie powiodło, false w przeciwnym wypadku
+	 * Ustawia informację o nieudanej aktualizacji
+	 * @param isFail true, gdy się nie powiodło, false w przeciwnym wypadku
 	 */
 	public void setFail(boolean isFail) {
 		this.isFail = isFail;
 	}
 	/**
-	 * Metoda zwraca tytul kanalu
-	 * @return tytul kanalu lub null, gdy tytul nie jest ustawiony
+	 * Zwraca tytuł kanału
+	 * @return tytuł kanału lub null, gdy tytuł nie jest ustawiony
 	 */
 	public String getTitle() {
 		return this.title;
 	}
 	/**
-	 * Metoda ustawia tytul kanalu
-	 * @param title - tytul kanalu na ktory chcemy zmienic
+	 * Ustawia tytuł kanału
+	 * @param title tytuł kanału na który chcemy zmienić
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 	/**
-	 * Metoda zwraca odnosnik do strony z ktorej zostal pobrany kanal
-	 * @return odnosnik do strony lub null, gdy odnosnik nie jest ustawiony
+	 * Zwraca odnośnik do strony z której został pobrany kanał
+	 * @return odnośnik do strony lub null, gdy odnośnik nie jest ustawiony
 	 */
 	public String getLink() {
 		return this.link;
 	}
 	/**
-	 * Metoda ustawia odnosnik do strony z ktorej zostal pobrany kanal
-	 * @param link - odnoscik do strony na ktory chcemy zmienic
+	 * Ustawia odnośnik do strony z której został pobrany kanał
+	 * @param link odnościk do strony na który chcemy zmienić
 	 */
 	public void setLink(String link) {
 		this.link = link;
 	}
 	/**
-	 * Metoda zwraca opis kanalu
-	 * @return opis kanalu lub null, gdy opis nie jest ustawiony
+	 * Zwraca opis kanału
+	 * @return opis kanału lub null, gdy opis nie jest ustawiony
 	 */
 	public String getDescription() {
 		return this.description;
 	}
 	/**
-	 * Metoda usawia opis kanalu
-	 * @param description - opis kanalu na ktory chcemy zmienic
+	 * Usawia opis kanału
+	 * @param description opis kanału na który chcemy zmienić
 	 */
 	public void setDescription(String description) {
 		this.description = description;
@@ -193,5 +211,19 @@ public class Channel implements Serializable {
 	/* nie wiem co to ma byc */
 	public void setImageLink(String imageLink) {
 		this.imageLink = imageLink;
+	}
+	/**
+	 * Zwraca ilość nieprzeczytanych elementów w kanale
+	 * @return ilość nieprzeczytanych elementów
+	 */
+	public int getUnreadItemsCount() {
+		return this.unreadItemsCount;
+	}
+	/**
+	 * Ustawia ilość nieprzeczytanych elementów w kanale
+	 * @param unreadItemsCount - ilość nieprzeczytanych elementów
+	 */
+	public void setUnreadItemsCount(int unreadItemsCount) {
+		this.unreadItemsCount = unreadItemsCount;
 	}
 }
