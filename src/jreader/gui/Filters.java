7f09db09b7de8edd3002019781e28c8453707938
kajoj87 <kajoj87@c9278602-9774-11dd-b35e-65caead18e0b>
2008-12-16 22:48:57 +0000

    stworzylem statyczne metody odswierzajace... zrobilem zmiane fontu gdy sie oznaczy item jako read, przy dodawaniu subskrypcji wylapuje wyjatki i wypisuje w okienku jaki byl problem, dorobilem popupmenu dla itemow z tabeli itemsTable

---
+++ src/jreader/gui/Filters.java
@@ -0,0 +1,109 @@
+package jreader.gui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import jreader.JReader;
+
+public class Filters {
+
+	public static CTabFolder folderFilter;
+	public static Button allButton;
+	public static Button unreadButton;
+	public String allButtonLabel = "All items ";
+	public String unreadButtonLabel = "Unread items ";
+	public int unread = 0;
+	public int msgs = 0;
+
+	public Filters(final Composite shell) {
+		
+		Display display = shell.getDisplay();
+		final Image rssTab = new Image(display, "c:\\icons\\filters\\rss-tab.png");
+		
+		folderFilter = new CTabFolder(shell, SWT.BORDER | SWT.SINGLE );
+		folderFilter.setSimple(JReader.issimple);
+		
+		  
+		Device device = Display.getCurrent ();
+		Color bottom = new Color (device, 156, 156, 213);
+		Color middle = new Color (device, 190, 190, 213);
+		  
+		//Ctab 
+		final CTabItem item = new CTabItem(folderFilter, SWT.NONE);
+		item.setText("Filters");
+		item.setImage(rssTab);
+		
+		/*
+		 * Zawartosc ctaba
+		 */
+		Composite filterComposite = new Composite(folderFilter, SWT.NONE);
+		filterComposite.setLayoutData(new FillLayout());
+		filterComposite.setLayout(new FillLayout(SWT.VERTICAL));
+		allButton = new Button(filterComposite, SWT.TOGGLE | SWT.FLAT);
+		allButton.setAlignment(SWT.LEFT);
+		allButton.setText(allButtonLabel + "(" + msgs + ")");
+		
+		unreadButton = new Button(filterComposite, SWT.TOGGLE | SWT.FLAT);
+		unreadButton.setAlignment(SWT.LEFT);
+		unreadButton.setText(unreadButtonLabel +  "(" + unread + ")");
+		unreadButton.setSelection(true);
+		
+		item.setControl(filterComposite);
+		/*
+		 * koniec zawartosci ctaba
+		 */
+		
+		
+		folderFilter.setSelection(item);
+		folderFilter.setSelectionBackground(new Color[]{display.getSystemColor(SWT.COLOR_WHITE), middle, bottom, bottom},
+				new int[] {20, 40, 100}, true);
+
+		  
+//		LISTENERS
+		
+		folderFilter.addMouseMoveListener(new MouseMoveListener() {
+            public void mouseMove(MouseEvent e) {
+              JReader.statusText = "Choose the type of messages filter.";
+              JReader.statusLine.setText(JReader.statusText);
+            }
+          });
+		allButton.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				if (allButton.getSelection()) {
+					unreadButton.setSelection(false);
+					allButton.setSelection(true);
+				}
+			}
+		});
+		unreadButton.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event event) {
+				if (unreadButton.getSelection()) {
+					allButton.setSelection(false);
+					unreadButton.setSelection(true);
+				}
+			}
+		});
+	}
+	
+	public void setBounds(Rectangle rect) {
+		folderFilter.setBounds(rect);
+	}
+	public void setBounds(int x, int y, int width, int height) {
+		folderFilter.setBounds(x, y, width, height);
+	}
+	public void setVisible(boolean bol) {
+		folderFilter.setVisible(bol);
+	}
+}
---
+++ src/jreader/gui/MainSash.java
@@ -0,0 +1,162 @@
+package jreader.gui;
+
+import jreader.JReader;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Sash;
+/**
+ * Główny kompozyt zawierający Previwe, Items, Filters, Tags.
+ *  
+ * @author Karol
+ *
+ */
+public class MainSash {
+
+	Composite sashComp;
+	Sash vSash;
+	Sash hSash;
+	Sash smallSash;
+	int SASH_LIMIT = 27;	//ogranicza z gory i z dolu
+	int SASH_WIDTH = 3;
+	int SMALL_SASH_LIMIT = 108; //ogranicza z gory
+	int folderFilterHeight = 80;
+	int folderItemHeight = 200;
+	static Filters folderFilter;
+	static Items folderItem;
+	static Preview folderPreview;
+	static Subscriptions folderSubs;
+	static Tags folderTag;
+	
+	public MainSash(Composite shell) {
+		
+		sashComp = new Composite(shell, SWT.NONE);
+		sashComp.setLayoutData (new GridData (SWT.FILL, SWT.FILL, true, true));
+		
+		/* Tworze nowe taby */
+		folderFilter = new Filters(sashComp); 
+		folderItem = new Items(sashComp);
+		folderPreview = new Preview(sashComp);
+		folderSubs = new Subscriptions(sashComp);
+		folderTag = new Tags(sashComp);
+		
+		/* Trzy sashes */
+		vSash = new Sash (sashComp, SWT.VERTICAL | SWT.SMOOTH);
+		hSash = new Sash (sashComp, SWT.HORIZONTAL | SWT.SMOOTH);
+		smallSash = new Sash(sashComp, SWT.HORIZONTAL | SWT.SMOOTH);
+		
+		/* listeners */
+		hSash.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				Rectangle rect = vSash.getParent().getClientArea();
+				event.y = Math.min (Math.max (event.y, SASH_LIMIT), rect.height - SASH_LIMIT);
+				if (event.detail != SWT.DRAG) {
+					hSash.setBounds (event.x, event.y, event.width, event.height);
+					layout ();
+				}
+			}
+		});
+		vSash.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				Rectangle rect = vSash.getParent().getClientArea();
+				//math.max ((event.x, OGRANICZENIE_Z_LEWEJ), rect.width - OGRANICZENIE_Z_PRAWEJ)
+				event.x = Math.min (Math.max (event.x, SMALL_SASH_LIMIT), rect.width - SMALL_SASH_LIMIT);
+				if (event.detail != SWT.DRAG) {
+					vSash.setBounds (event.x, event.y, event.width, event.height);
+					layout ();
+				}
+			}
+		});
+		smallSash.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				Rectangle rect = vSash.getParent().getClientArea();
+				event.y = Math.min (Math.max (event.y, SMALL_SASH_LIMIT), rect.height - SASH_LIMIT);
+				if (event.detail != SWT.DRAG) {
+					smallSash.setBounds (event.x, event.y, event.width, event.height);
+					layout ();
+				}
+			}
+		});
+		sashComp.addControlListener (new ControlAdapter () {
+			public void controlResized (ControlEvent event) {
+				resized ();
+			}
+		});
+		//'wyzerowanie' statusline
+		sashComp.addMouseMoveListener(new MouseMoveListener() {
+            public void mouseMove(MouseEvent e) {
+              JReader.statusText = "";
+              JReader.statusLine.setText(JReader.statusText);
+            }
+          });
+	}
+		
+	void layout () {
+		/*
+		 * Tu sie dzieje taka masakra ze nawet nie chce mi sie tego komentowac...
+		 * Jak masz odwage to sam to rozkmin.
+		 */
+		
+		Rectangle clientArea = sashComp.getClientArea ();
+		Rectangle hSashBounds = hSash.getBounds ();
+		Rectangle vSashBounds = vSash.getBounds ();
+		Rectangle smallSashBounds = smallSash.getBounds();
+		
+		folderFilter.setBounds (0, 0, vSashBounds.x, folderFilterHeight);
+		folderItem.setBounds (vSashBounds.x + vSashBounds.width, 0, clientArea.width - (vSashBounds.x + vSashBounds.width), hSashBounds.y);
+		folderPreview.setBounds (vSashBounds.x + vSashBounds.width, hSashBounds.y + hSashBounds.height, clientArea.width - (vSashBounds.x + vSashBounds.width), clientArea.height - (hSashBounds.y + hSashBounds.height));
+		folderSubs.setBounds (0, folderFilterHeight + SASH_WIDTH, vSashBounds.x, smallSashBounds.y - folderFilterHeight - smallSashBounds.height);
+		folderTag.setBounds(0, smallSashBounds.y + smallSashBounds.height, vSashBounds.x, clientArea.height - smallSashBounds.y - smallSashBounds.height);
+		
+		hSashBounds.width = clientArea.width - vSashBounds.x;
+		hSashBounds.x = vSashBounds.x;
+		smallSashBounds.width = vSashBounds.x;
+		
+		hSash.setBounds(hSashBounds);
+		smallSash.setBounds(smallSashBounds);
+	}
+	
+	void resized () {
+		
+		Rectangle clientArea = sashComp.getClientArea ();
+		
+		/*
+		* Tworzy bounds dla tabu 'filters'. Umieszcza go po lewej stronie.
+		* Wysoko�� z g�ry ustalona.
+		* (x, y, width, height)
+		*/
+		Rectangle list1Bounds = new Rectangle (0, 0, (clientArea.width - SASH_WIDTH) / 5, folderFilterHeight);
+		folderFilter.setBounds (list1Bounds);
+		/*
+		* Tworzy tab Items gora z prawej.
+		*/
+		folderItem.setBounds (list1Bounds.width + SASH_WIDTH, 0, clientArea.width - (list1Bounds.width + SASH_WIDTH), folderItemHeight);
+		/*
+		* Tworzy tab Preview tak samo jak items tyle ze na dole
+		*/
+		folderPreview.setBounds (list1Bounds.width + SASH_WIDTH, folderItemHeight + SASH_WIDTH, clientArea.width - (list1Bounds.width + SASH_WIDTH), clientArea.height - (folderItemHeight + SASH_WIDTH));
+		/*
+		 * Tworzy tab Subscryptions z lewej
+		 */
+		folderSubs.setBounds(0, folderFilterHeight + SASH_WIDTH, (clientArea.width - SASH_WIDTH) / 5, ((clientArea.height - folderFilterHeight) /3) *2 );
+		list1Bounds = folderSubs.getBounds();
+		/*
+		 * Tworzy tab Tags z lewej w dolnym rogu
+		 */
+		folderTag.setBounds(0, list1Bounds.height + folderFilterHeight + 2*SASH_WIDTH, (clientArea.width - SASH_WIDTH) / 5, clientArea.height - folderFilterHeight - list1Bounds.height - 2*SASH_WIDTH);
+		
+		/* Pozycjonowanie sashes */
+		vSash.setBounds (list1Bounds.width, 0, SASH_WIDTH, clientArea.height);
+		hSash.setBounds (list1Bounds.width, folderItemHeight, clientArea.width - list1Bounds.width, SASH_WIDTH);
+		smallSash.setBounds(0, list1Bounds.height + folderFilterHeight + SASH_WIDTH, (clientArea.width - SASH_WIDTH) / 5, SASH_WIDTH);
+	}
+	
+	
+}
