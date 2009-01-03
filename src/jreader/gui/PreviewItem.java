5f980840d5421225e409480b131c1132e1b899a2
kajoj87 <kajoj87@c9278602-9774-11dd-b35e-65caead18e0b>
2009-01-03 13:24:15 +0000

    nie dodalem jednej klasy..

---
+++ src/jreader/gui/PreviewItem.java
@@ -0,0 +1,81 @@
+package jreader.gui;
+
+import java.util.Date;
+
+import jreader.JReader;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
+
+public class PreviewItem {
+
+	public static Browser browser;
+	public static Label title;
+	public static Label author;
+	public static Link titleLink;
+	
+public PreviewItem(String text, Image itemImage) {
+		  
+		  
+		final CTabItem previewItem = new CTabItem(Preview.folderPreview, SWT.CLOSE);
+		previewItem.setText(text);
+		previewItem.setImage(itemImage);
+		Composite comp = new Composite(Preview.folderPreview, SWT.NONE);
+		comp.setLayout(new GridLayout());
+		Composite header = new Composite(comp, SWT.NONE);
+		header.setLayout(new FillLayout(SWT.VERTICAL));
+		header.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		titleLink = new Link(header, SWT.NONE);
+		title = new Label(header, SWT.NONE);
+		author = new Label(header, SWT.NONE);
+		
+		
+		
+		if (System.getProperty("os.name").equalsIgnoreCase("Linux")) {
+			browser = new Browser(comp, SWT.MOZILLA | SWT.BORDER);
+		} else {
+			browser = new Browser(comp, SWT.NONE | SWT.BORDER);
+		}
+		browser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		
+		previewItem.setControl(comp);
+		Preview.folderPreview.setSelection(previewItem);
+		refreshItem();
+		
+	}
+
+	public static void refreshItem() {
+	String titleText = JReader.getPreview().getCurrent().getTitle();
+	Date date = JReader.getPreview().getCurrent().getDate();
+	String authorText = JReader.getPreview().getCurrent().getAuthor();
+	String fromText = JReader.getPreview().getCurrent().getChannelTitle();
+	titleLink.setText("<a>" + titleText + "</a>");
+	
+	browser.setText(JReader.getPreview().getCurrent().getHTML());
+	title.setText(((date != null) ? date.toString() : " "));
+	if (authorText != null && fromText != null)
+		author.setText("Author: " + authorText + "\tFrom: " + fromText);
+	else if (fromText != null && authorText == null)
+		author.setText("From: " + fromText);
+	else if (authorText != null)
+		author.setText(authorText);
+	else
+		author.setText("");
+	
+	titleLink.addListener (SWT.Selection, new Listener () {
+		public void handleEvent(Event event) {
+			browser.setUrl(JReader.getPreview().getCurrent().getLink());
+		}
+	});
+}
+}
