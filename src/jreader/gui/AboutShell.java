1161e7c57f743981e810435b2e1e81099a5206c2
kajoj87 <kajoj87@c9278602-9774-11dd-b35e-65caead18e0b>
2009-01-08 23:46:42 +0000

    dodane spacje w about

--- src/jreader/gui/AboutShell.java
+++ src/jreader/gui/AboutShell.java
@@ -31,19 +31,19 @@ public class AboutShell {
 			"<a href='http://code.google.com/p/jreader-ug/'>http://code.google.com/p/jreader-ug/</a>" +
 			"<br /><br />" +
 			"<p>" +
-			"This program is free software: you can redistribute it and/or modify" +
-			"it under the terms of the GNU General Public License as published by" +
-			"the Free Software Foundation, either version 3 of the License, or" +
+			"This program is free software: you can redistribute it and/or modify " +
+			"it under the terms of the GNU General Public License as published by " +
+			"the Free Software Foundation, either version 3 of the License, or " +
 			"(at your option) any later version." +
 			"</p>" +
 			"<p>" +
-			"This program is distributed in the hope that it will be useful," +
-			"but WITHOUT ANY WARRANTY; without even the implied warranty of" +
-			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the" +
+			"This program is distributed in the hope that it will be useful, " +
+			"but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the " +
 			"GNU General Public License for more details." +
 			"</p>" +
 			"<p>" +
-			"You should have received a copy of the GNU General Public License" +
+			"You should have received a copy of the GNU General Public License " +
 			"along with this program.  If not, see <a href='http://www.gnu.org/licenses/'>" +
 			"http://www.gnu.org/licenses/</a>" +
 			"</p></div>" +
