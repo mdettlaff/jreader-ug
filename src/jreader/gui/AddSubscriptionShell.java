bb412914d91106b8306623852eb0396f8eeabb30
kajoj87 <kajoj87@c9278602-9774-11dd-b35e-65caead18e0b>
2009-01-08 20:45:13 +0000

    mozna zatwierdzac enterem w oknach dodwania subskrypcji i tagu

--- src/jreader/gui/AddSubscriptionShell.java
+++ src/jreader/gui/AddSubscriptionShell.java
@@ -9,11 +9,13 @@ import jreader.JReader;
 import jreader.LinkNotFoundException;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
@@ -27,12 +29,13 @@ public class AddSubscriptionShell {
 
 	private static Shell addShell;
 	private static Label warning;
+	private static Text url;
+	private static Text tag;
 	
 	public AddSubscriptionShell(Shell shell) {
 		final Image jreader = new Image(shell.getDisplay(), "data" + File.separator + "icons" + File.separator + "small" + File.separator + "add.png");
 		addShell = new Shell(shell, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 		addShell.setText("Add a subscription");
-		//aboutShell.setSize(200, 150);
 		addShell.setImage(jreader);
 		addShell.setLayout(new GridLayout(2, true));
 		
@@ -45,17 +48,19 @@ public class AddSubscriptionShell {
 	    
 	    
 	    new Label(addShell, SWT.NONE).setText("Give the URL address: ");
-	    final Text url = new Text(addShell, SWT.BORDER);
+	    url = new Text(addShell, SWT.BORDER);
 	    url.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 	    url.setTextLimit(100);
+	    url.setFocus();
 	    
 	    new Label(addShell, SWT.NONE).setText("Name the tags (optional): ");
-	    final Text tag = new Text(addShell, SWT.BORDER);
+	    tag = new Text(addShell, SWT.BORDER);
 	    tag.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 	    tag.setTextLimit(100);
 	    
 	    warning = new Label(addShell, SWT.NONE);
 	    warning.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+	    warning.setForeground(new Color(Display.getCurrent(), 255, 0, 0));
 	    
 	    Button okB = new Button(addShell, SWT.PUSH);
 	    okB.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
@@ -71,40 +76,7 @@ public class AddSubscriptionShell {
 	    //OK button
 	    okB.addListener(SWT.Selection, new Listener() {
             public void handleEvent(Event event) {
-            	GUI.statusLine.setText("");
-            	System.out.println(url.getText());
-            	System.out.println(tag.getText());
-				try {
-					JReader.addChannel(url.getText(), tag.getText());
-				} catch (LinkNotFoundException lnfe) {
-					System.out.println("Nie znaleziono kanalow na tej stronie.");
-					warning.setText("Rss not found on this site.");
-					return;
-				} catch (MalformedURLException mue) {
-					System.out.print("Nie mozna dodac kanalu.");
-					System.out.println(" URL jest nieprawidlowy.");
-				} catch (SAXParseException spe) {
-					System.out.print("Nie mozna dodac kanalu.");
-					System.out.println(" Zrodlo nie jest prawidlowym plikiem XML.");
-					System.out.print("Blad w linii " + spe.getLineNumber() + ". ");
-					System.out.println("Szczegoly: " + spe.getLocalizedMessage());
-				} catch (SAXException saxe) {
-					System.out.print("Nie mozna dodac kanalu.");
-					System.out.println(" Blad parsera XML.");
-				} catch (SocketException se) {
-					System.out.println("Nie mozna dodac kanalu. Szczegoly:");
-					System.out.println(se.getLocalizedMessage());
-				} catch (IOException ioe) {
-					System.out.print("Nie mozna dodac kanalu.");
-					System.out.println(" Pobieranie strony nie powiodlo sie.");
-				} catch (IllegalArgumentException iae) {
-					System.out.println("URL jest nieprawidłowy");
-					warning.setText("Wrong URL.");
-					return;
-				}
-				SubsList.refresh();
-				TagList.refresh();
-            	addShell.dispose();
+            		addUrl();
                 }
         });
 	    caB.addListener(SWT.Selection, new Listener() {
@@ -113,11 +85,59 @@ public class AddSubscriptionShell {
             	addShell.dispose();
                 }
         });
-	    
+	    url.addListener(SWT.DefaultSelection, new Listener() {
+	        public void handleEvent(Event e) {
+	        	addUrl();
+	        }
+	      });
+	    tag.addListener(SWT.DefaultSelection, new Listener() {
+	        public void handleEvent(Event e) {
+	        	addUrl();
+	        }
+	      });
 	    
 	    addShell.pack();
 	}
 
+	public static void addUrl() {
+		String warn = new String();
+		try {
+			JReader.addChannel(url.getText(), tag.getText());
+		} catch (LinkNotFoundException lnfe) {
+			warning.setText("Rss not found on this site.");
+			return;
+		} catch (MalformedURLException mue) {
+			warning.setText("Wrong URL.");
+			return;
+		} catch (SAXParseException spe) {
+			warn = "Source is not a valid XML file.\n";
+			warn += "Error at line " + spe.getLineNumber() + ".\n";
+			warn += "Details: " + spe.getLocalizedMessage() + ".\n";
+			warning.setText(warn);
+			return;
+		} catch (SAXException saxe) {
+			warn = "XML's parser error.\n";
+			warning.setText(warn);
+			return;
+		} catch (SocketException se) {
+			warn = "Cannot add the channel.\n";
+			warn += "Detail: " + se.getLocalizedMessage();
+			warning.setText(warn);
+			return;
+		} catch (IOException ioe) {
+			warn = "Site download failed.";
+			warning.setText(warn);
+			return;
+		} catch (IllegalArgumentException iae) {
+			warn = "Wrong URL";
+			warning.setText(warn);
+			return;
+		}
+		SubsList.refresh();
+		TagList.refresh();
+    	addShell.dispose();
+	}
+	
 	public void open() {
 		addShell.open();
 	}
